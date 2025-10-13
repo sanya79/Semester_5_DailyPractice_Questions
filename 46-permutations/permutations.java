ee3af0adafa21736562b40fc9f4167b04a6dbf06
Sanya Katiyar <katiyarsanya923@gmail.com>
2025-10-14 01:55:20 +0530

    Time: 1 ms (93.02%) | Memory: 44.4 MB (83.04%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Solution {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, new boolean[nums.length], new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void backtrack(int[] nums, boolean[] used, List<Integer> path, List<List<Integer>> res) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            path.add(nums[i]);
+            backtrack(nums, used, path, res);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+        List<List<Integer>> result = permute(nums);
+        for (List<Integer> perm : result) System.out.println(perm);
+        sc.close();
+    }
+}
