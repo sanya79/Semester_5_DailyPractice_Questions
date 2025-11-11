0cdb2f927b798453978b9afaf27403d78a716b73
Sanya Katiyar <katiyarsanya923@gmail.com>
2025-11-12 00:20:09 +0530

    Time: 1 ms (70.53%) | Memory: 42.4 MB (16.96%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int k = 0;
+        String repeated = word;
+        while (sequence.contains(repeated)) {
+            k++;
+            repeated += word;
+        }
+        
+        return k;
+    }
+}
