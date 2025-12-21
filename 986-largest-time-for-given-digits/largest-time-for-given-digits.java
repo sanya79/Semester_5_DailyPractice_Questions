5ae70edaeee275360f1c6e3a357bce650ee96c70
Sanya Katiyar <katiyarsanya923@gmail.com>
2025-12-21 14:40:38 +0530

    Time: 5 ms (81.06%) | Memory: 43.3 MB (56.39%) - LeetSync

--- 986-largest-time-for-given-digits/largest-time-for-given-digits.java
+++ 986-largest-time-for-given-digits/largest-time-for-given-digits.java
@@ -1,36 +1,25 @@
+import java.util.*;
 class Solution {
-    
-    private boolean rearrange(int maxValue,int index,int[] a){
-        int max = -1;
-		
-		// find the maximum number for the given index which should be less than equal
-		// to maxValue
-        for(int i = index; i<a.length;i++) if(a[i]<=maxValue && (max==-1 || a[max]<a[i])) max = i;
-		// number not found
-        if(max==-1) return false;
-    
-	// put the number to its correct index 
-	// swap it put at correct position
-        int temp = a[max];
-        a[max] = a[index];
-        a[index] = temp;
-        return true;
-    }
-	
-	
-    
-    public String largestTimeFromDigits(int[] a) {
-        boolean res = (rearrange(2,0,a) && (a[0]==2 ? rearrange(3,1,a) : rearrange(9,1,a)) && rearrange(5,2,a) && rearrange(9,3,a)) || (rearrange(1,0,a) && rearrange(9,1,a) && rearrange(5,2,a) && rearrange(9,3,a));
-        
-        
-        StringBuilder sb = new StringBuilder();
-        
-        if(!res) return sb.toString();
-        
-        return sb.append(String.valueOf(a[0]))
-          .append(String.valueOf(a[1]))
-          .append(':')
-          .append(String.valueOf(a[2]))
-          .append(String.valueOf(a[3])).toString();
+    public String largestTimeFromDigits(int[] arr) {
+        int maxTime = -1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (j == i) continue;
+                for (int k = 0; k < 4; k++) {
+                    if (k == i || k == j) continue;
+                    int l = 6 - i - j - k; 
+                    int hour = arr[i] * 10 + arr[j];
+                    int min = arr[k] * 10 + arr[l];
+                    if (hour < 24 && min < 60) {
+                        int totalMinutes = hour * 60 + min;
+                        maxTime = Math.max(maxTime, totalMinutes);
+                    }
+                }
+            }
+        }
+        if (maxTime == -1) return "";
+        int maxHour = maxTime / 60;
+        int maxMin = maxTime % 60;
+        return String.format("%02d:%02d", maxHour, maxMin);
     }
 }
