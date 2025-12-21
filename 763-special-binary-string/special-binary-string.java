9ef9a5db9c59fd01c6d5cbfbdb946da5fb02485b
Sanya Katiyar <katiyarsanya923@gmail.com>
2025-12-21 14:41:37 +0530

    Time: 2 ms (100.00%) | Memory: 43.6 MB (5.35%) - LeetSync

--- 763-special-binary-string/special-binary-string.java
+++ 763-special-binary-string/special-binary-string.java
@@ -1,34 +1,28 @@
 import java.util.*;
-
 class Solution {
     public String makeLargestSpecial(String s) {
-        List<String> parts = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         int count = 0;
         int start = 0;
-
-        // Split into primitive special substrings
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') count++;
-            else count--;
-
+            if (s.charAt(i) == '1') {
+                count++;
+            } else {
+                count--;
+            }
             if (count == 0) {
-                // Recursively process inner part
                 String inner = s.substring(start + 1, i);
-                String processed = "1" + makeLargestSpecial(inner) + "0";
-                parts.add(processed);
+                String processed = makeLargestSpecial(inner);
+                list.add("1" + processed + "0");
                 start = i + 1;
             }
         }
+        Collections.sort(list, Collections.reverseOrder());
 
-        // Sort in descending lexicographical order
-        Collections.sort(parts, Collections.reverseOrder());
-
-        // Combine result
         StringBuilder result = new StringBuilder();
-        for (String part : parts) {
-            result.append(part);
+        for (String str : list) {
+            result.append(str);
         }
-
         return result.toString();
     }
 }
