f685fbd98c7fa0e8535b304dd391611e88d27bc1
Sanya Katiyar <katiyarsanya923@gmail.com>
2026-02-02 10:33:40 +0530

    Time: 26 ms (35.65%) | Memory: 82.2 MB (72.61%) - LeetSync

---
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -0,0 +1,27 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        Deque<Integer> deque = new LinkedList<>();
+        deque.offer(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() < i - k) {
+                deque.pollFirst();
+            }
+            dp[i] = dp[deque.peekFirst()] + nums[i];
+            while (!deque.isEmpty() && dp[deque.peekLast()] <= dp[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+        }
+
+        return dp[n - 1];
+    }
+}
