9a23a47d56acbedfec796c4cb030422276764de2
Sanya Katiyar <katiyarsanya923@gmail.com>
2026-02-11 12:46:12 +0530

    Time: 360 ms (96.88%) | Memory: 159.6 MB (97.72%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int findMaximumXOR(int[] arr) {
+        Trie t=new Trie();
+        for(int x:arr){
+            t.add(x);
+        }
+        int ans=0;
+        for(int x:arr){
+            ans=Math.max(t.getMaxXor(x),ans);
+        }
+        return ans;
+    }
+    static class Trie{
+        class Node{
+            Node zero;
+            Node one;
+        }
+        private Node root=new Node();
+        public void add(int val){
+            Node curr=root;
+            for(int i=31;i>=0;i--){
+                int bit=val & (1<<i);
+                if(bit==0){
+                    if(curr.zero!=null){
+                        curr=curr.zero;
+                    }
+                    else{
+                        Node nn=new Node();
+                        curr.zero=nn;
+                        curr=nn;
+                    }
+                }
+                else{
+                    if(curr.one !=null){
+                        curr=curr.one;
+                    }
+                    else{
+                        Node nn=new Node();
+                        curr.one=nn;
+                        curr=nn;
+                    }
+                }
+            }
+        }
+        public int getMaxXor(int x){
+            int ans=0;
+            Node curr=root;
+            for(int i=31;i>=0;i--){
+                int bit=x & (1<<i);
+                if(bit==0){
+                    if(curr.one !=null){
+                        ans=ans | (1<<i);
+                        curr=curr.one;
+                    }
+                    else{
+                        curr=curr.zero;
+                    }
+                }
+                else{
+                    if(curr.zero !=null){
+                        ans=ans | (1<<i);
+                        curr=curr.zero;
+                    }
+                    else{
+                        curr=curr.one;
+                    }
+                }
+            }
+            return ans;
+        } 
+    }
+}
