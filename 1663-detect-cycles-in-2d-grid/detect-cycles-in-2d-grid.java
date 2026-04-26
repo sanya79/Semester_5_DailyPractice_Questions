f327eeb596fc226be825aa919a9360e4f00f51c9
Sanya Katiyar <katiyarsanya923@gmail.com>
2026-04-26 17:16:50 +0530

    Time: 20 ms (50.00%) | Memory: 139.8 MB (60.75%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -1,15 +1,15 @@
 class Solution {
-    private int rows, cols;
+    private int r, c;
     private char[][] grid;
-    private boolean[][] visited;
+    private boolean[][] v;
     public boolean containsCycle(char[][] grid) {
         this.grid = grid;
-        rows = grid.length;
-        cols = grid[0].length;
-        visited = new boolean[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (!visited[i][j]) {
+        r = grid.length;
+        c = grid[0].length;
+        v = new boolean[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (!v[i][j]) {
                     if (dfs(i, j, -1, -1, grid[i][j])) {
                         return true;
                     }
@@ -19,13 +19,13 @@ class Solution {
         return false;
     }
     private boolean dfs(int x, int y, int pX, int pY, char ch) {
-        visited[x][y] = true;
+        v[x][y] = true;
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
         for (int k = 0; k < 4; k++) {
             int nx = x + dx[k];
             int ny = y + dy[k];
-            if (nx < 0 || ny < 0 || nx >= rows || ny >= cols) {
+            if (nx < 0 || ny < 0 || nx >= r || ny >= c) {
                 continue;
             }
             if (grid[nx][ny] != ch) {
@@ -34,7 +34,7 @@ class Solution {
             if (nx == pX && ny == pY) {
                 continue;
             }
-            if (visited[nx][ny]) {
+            if (v[nx][ny]) {
                 return true;
             }
             if (dfs(nx, ny, x, y, ch)) {
