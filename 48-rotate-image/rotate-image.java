2a468da0550afa9932e35fe7495b699b9b698ded
Sanya Katiyar <katiyarsanya923@gmail.com>
2026-05-04 20:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (84.65%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,47 +1,31 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
+    public void rotate(int[][] m) {
+        int n = m.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                int t = m[i][j];
+                m[i][j] = m[j][i];
+                m[j][i] = t;
             }
         }
         for (int i = 0; i < n; i++) {
-            reverseRow(matrix, i);
+            reverseRow(m, i);
         }
     }
-    private void reverseRow(int[][] matrix, int row) {
-        int left = 0;
-        int right = matrix.length - 1;
-        while (left < right) {
-            int temp = matrix[row][left];
-            matrix[row][left] = matrix[row][right];
-            matrix[row][right] = temp;
-            left++;
-            right--;
+    private void reverseRow(int[][] matrix, int r) {
+        int l = 0;
+        int ri = matrix.length - 1;
+        while (l < ri) {
+            int t = matrix[r][l];
+            matrix[r][l] = matrix[r][ri];
+            matrix[r][ri] = t;
+            l++;
+            ri--;
         }
     }
-    // public static void main(String[] args) {
-    //     Solution solution = new Solution();
-
-    //     // Example test case 1
-    //     int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    //     solution.rotate(matrix1);
-    //     printMatrix(matrix1); // Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-    //     int[][] matrix2 = {
-    //         {5, 1, 9, 11},
-    //         {2, 4, 8, 10},
-    //         {13, 3, 6, 7},
-    //         {15, 14, 12, 16}
-    //     };
-    //     solution.rotate(matrix2);
-    //     printMatrix(matrix2); 
-    // }
     private static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int val : row) {
+        for (int[] r : matrix) {
+            for (int val : r) {
                 System.out.print(val + " ");
             }
             System.out.println();
