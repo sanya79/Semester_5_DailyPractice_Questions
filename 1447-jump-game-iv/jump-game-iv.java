a40e1dd63880997fbd4b1af368fcde02f28d59a7
Sanya Katiyar <katiyarsanya923@gmail.com>
2026-05-18 12:38:51 +0530

    Time: 47 ms (92.64%) | Memory: 80.2 MB (67.99%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+        q.offer(0);
+        vis[0] = true;
+        int steps = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int i = q.poll();
+                if (i == n - 1) return steps;
+                if (i + 1 < n && !vis[i + 1]) {
+                    vis[i + 1] = true;
+                    q.offer(i + 1);
+                }
+                if (i - 1 >= 0 && !vis[i - 1]) {
+                    vis[i - 1] = true;
+                    q.offer(i - 1);
+                }
+                if (map.containsKey(arr[i])) {
+                    for (int j : map.get(arr[i])) {
+                        if (!vis[j]) {
+                            vis[j] = true;
+                            q.offer(j);
+                        }
+                    }
+                    map.remove(arr[i]); 
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+}
